d8418b4f43fe2228a297b175bf423b928a15a1dd
¨desenho¨ <desenhojp@gmail.com>
2022-11-23 15:57:48 -0300

    terceiro exercicio - if-else

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/scripts.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,9 +1,11 @@
-const base = 5;
+const nota = 40;
 
-const heigth = 8;
-
-const area = base * heigth;
-
-const perimeter = ( 5+5+8+8)
-console.log(area);
-console.log(perimeter);
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (nota < 80 && nota >= 60){
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada(o)");
+}
