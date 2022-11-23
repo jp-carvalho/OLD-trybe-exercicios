e011d7121fd3fc48713207db0a0682884927e821
¨desenho¨ <desenhojp@gmail.com>
2022-11-23 15:10:55 -0300

    primeiro exercicioExercício Variáveis

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,11 @@
+let myName = "Joao Paulo";
+const birthCity = "Rio de Janeiro";
+let birtYear = 1984;
+
+birtYear = 2030;
+
+// birthCity = "Angola";
+
+console.log(myName, birthCity, birtYear);
+
+// const não se altera valor
