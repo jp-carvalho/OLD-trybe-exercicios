1c309e27891a3ab3ddd0a31614307eab32554546
¨desenho¨ <desenhojp@gmail.com>
2022-11-23 18:59:43 -0300

    exercicio 1 e 2

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,29 @@
+// exercicio 1
+// const num1 = 10;
+
+// const num2 = 20;
+
+// if ( num1 > num2 ){
+//     console.log( num1 );
+// }
+// else {
+//     console.log( num2 );
+// }
+
+// exercicio 2
+const num1 = 10;
+
+const num2 = 20;
+
+const num3 = 45;
+
+if ( num1 > num2 && num1 > num3){
+    console.log( num1 );
+}
+else if ( num2 > num1 && num2 > num3 ) {
+    console.log( num2 );
+}
+
+else {
+    console.log( num3 );
+}
