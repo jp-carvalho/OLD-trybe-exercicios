8744f212016a40b5fb0c049c045452a4d3df0341
¨desenho¨ <desenhojp@gmail.com>
2022-11-26 15:43:14 -0300

    exercicios course no scriptCourse.js 2

---
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/dia-02-array-e-loop-for/scripts.js
@@ -0,0 +1,32 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// exercicio1
+// for(i = 0; index < numbers.length; index += 1){
+// console.log(numbers[index]);
+// }
+
+// exercicio2
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     sum += numbers[i];
+// }
+// console.log(sum);
+
+// exercicio3
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     sum += numbers[i];
+// }
+// console.log(sum / numbers.length);
+
+// exercicio4
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     sum += numbers[i];
+// }
+// console.log(sum / numbers.length);
+
+// exercicio5
