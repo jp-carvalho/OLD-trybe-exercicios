633010739a1dcf655bb484d8e421d80d70cb4331
¨desenho¨ <desenhojp@gmail.com>
2022-12-01 09:57:53 -0300

    resolução exercicios bonus fundamentos/4.2 Bubble sort

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/dia-02-array-e-loop-for/scripts.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/dia-01-javascript-primeiros-passos/dia-02-array-e-loop-for/scripts.js
@@ -83,11 +83,58 @@
 
 //exercicio 9
 // Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
-let array = [];
+// let array = [];
 
-for (let index = 1; index <= 25; index += 1) {
-    array.push(index);
-}
-for (let index2 = 0; index2 < array.length; index2 +=1) {
-    console.log(array[index2] / 2);
-}
+// for (let index = 1; index <= 25; index += 1) {
+//     array.push(index);
+// }
+// for (let index2 = 0; index2 < array.length; index2 +=1) {
+//     console.log(array[index2] / 2);
+// }
+
+//exercicio Bonus
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//exercicio 01
+//Ordene o array numbers em ordem crescente e imprima seus valores;
+// for (index = 1; index < numbers.length; index += 1) {
+//     for (secondIndex = 0; secondIndex < index; secondIndex += 1){
+//         if (numbers[index] < numbers[secondIndex]) {
+//             position = numbers[index];
+//             numbers[index] = numbers[secondIndex];
+//             numbers[secondIndex] = position;
+//         }
+//     }
+// }
+// console.log(numbers);
+
+//exercicio 02
+//Ordene o array numbers em ordem decrescente e imprima seus valores;
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (index = 1; index < numbers.length; index += 1){
+//     for( secondIndex = 0; secondIndex < index; secondIndex += 1){
+//         if(numbers[index] > numbers[secondIndex]){
+//             position = numbers[index];
+//             numbers[index] = numbers[secondIndex];
+//             numbers[secondIndex] = position;
+//         }
+//     }
+// }
+// console.log(numbers);
+
+//exercicio 03
+//Agora, crie um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. O resultado deve ser o array abaixo:
+// [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let multiply = [];
+
+for(index = 0; index < numbers.length; index += 1) {
+    if (index +1 < numbers.length){
+        multiply.push(numbers[index] * numbers[index + 1]);
+    }else {
+        multiply.push(numbers[index] * 2);
+    }
+} 
+console.log(multiply);
