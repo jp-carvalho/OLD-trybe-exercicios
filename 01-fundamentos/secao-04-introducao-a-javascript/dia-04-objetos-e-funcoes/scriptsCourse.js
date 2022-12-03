e3c80487f86453fbb3e2b70177736e81d45e7418
¨desenho¨ <desenhojp@gmail.com>
2022-12-03 11:28:48 -0300

    Para fixar - For/in e For/off

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/scriptsCourse.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/scriptsCourse.js
@@ -1,17 +1,46 @@
-let player ={
-    name: 'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals: {
-        golden: 2,
-        silver: 3,
-    }
-};
+// Para fixar - Objetos
 
-player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+// let player ={
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: {
+//         golden: 2,
+//         silver: 3,
+//     }
+// };
 
-// console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+// player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 
-console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes.');
+// // console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
 
-console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
+// console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes.');
+
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
+
+// Para fixar - For/in e For/off
+
+// /Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+
+// for (let key in names){
+//     console.log('Olá ' + names[key]);
+// }
+
+
+//Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+for ( let key in car) {
+    console.log(key, car[key]);
+}
