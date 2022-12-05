0a98ff9a285844113af98418539fbd9ca7126c3a
¨desenho¨ <desenhojp@gmail.com>
2022-12-05 12:37:10 -0300

    exercicios Bonus funções - Todos com gabarito

--- 01-fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/scripts.js
+++ 01-fundamentos/secao-04-introducao-a-javascript/dia-04-objetos-e-funcoes/scripts.js
@@ -230,20 +230,208 @@
 
 // Retorno esperado: false
 
-function check(word, ending){
-  word = word.split('');
-  ending = ending.split('');
+// function check(word, ending){
+//   word = word.split('');
+//   ending = ending.split('');
 
-  let checkEnding = true;
+//   let checkEnding = true;
 
-  for (let index = 0; index < ending.length; index += 1) {
-    if (word[word.length - ending.length + index] != ending[index]) {
-      checkEnding = false;
-    }
-  }
+//   for (let index = 0; index < ending.length; index += 1) {
+//     if (word[word.length - ending.length + index] != ending[index]) {
+//       checkEnding = false;
+//     }
+//   }
 
-  return checkEnding;
+//   return checkEnding;
+// }
+
+// console.log(check('trybe', 'be'));
+// console.log(check('joaofernando', 'fernan'));
+
+
+// Exercicio Bonus 01
+// (Bônus) Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
+
+// Atenção! Esse exercício já apareceu no processo seletivo de uma grande multinacional!
+
+// Dicas:
+
+// Uma string é um array de caracteres, então cada elemento do array é uma letra.
+// O valor de cada numeral romano é:
+
+// | I   | 1    |
+// | --- | ---- |
+// | IV  | 4    |
+// | V   | 5    |
+// | IX  | 9    |
+// | X   | 10   |
+// | XL  | 40   |
+// | L   | 50   |
+// | XC  | 90   |
+// | C   | 100  |
+// | CD  | 400  |
+// | D   | 500  |
+// | CM  | 900  |
+// | M   | 1000 |
+
+// Que tal criar um objeto que associe cada letra a um numeral para fácil consulta?
+
+// Atenção! Quando você tem um número pequeno à direita de um número grande, eles devem ser somados. Exemplo: XI = 10 + 1 = 11. No entanto, se o número pequeno está à esquerda de um número maior que ele, ele deve ser subtraído. Exemplo: IX = 10 - 1 = 9.
+
+// const numerosRomanos = {
+//     i: 1,
+//     v: 5,
+//     x: 10,
+//     l: 50,
+//     c: 100,
+//     d: 500,
+//     m: 1000,
+// };
+
+// function converteRomano(number) {
+//   number = number.toLowerCase();
+//   const len = number.length;
+//   let sum = numerosRomanos[number[len -1]];
+//   let atual = numerosRomanos[number[len - 1]];
+
+//   for (let index =2; index <= len; index +=1) {
+//     const proximo = numerosRomanos[number[len - index]];
+
+//     if (atual <= proximo) {
+//       sum += proximo;
+//     } else {
+//       sum -= proximo;
+//     }
+
+//     atual = proximo;
+//   }
+
+//   return sum;
+// }
+
+// console.log(converteRomano('MMXVIII'));
+// console.log(converteRomano('VI'));
+// console.log(converteRomano('IV'));
+
+
+// exercicio Bonus 02
+//Crie uma função chamada arrayOfNumbers que receberá a variável vector como parâmetro.
+
+// Através de um loop for, percorra essa variável, busque os números pares e os adicione a um novo array, que deverá ser retornado ao final pela função.
+
+// let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
+
+// function arrayOfNumbers(vector) {
+//   const result = [];
+//   for (let index = 0; index < vector.length; index += 1){
+//     const numbers = vector[index];
+
+//     for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+//       const current = numbers[index2];
+
+//       if ((current % 2) === 0) {
+//         result.push(current);
+//       } 
+//     }
+//   }
+
+//   return result;
+// }
+
+// console.log(arrayOfNumbers(vector));
+
+
+//exercicio Bonus 03
+//A partir do array de frutas basket, retorne um objeto que contenha o nome da fruta como chave e a quantidade de vezes que ela aparece no array como valor. Por exemplo, o array [‘Melancia’, ‘Abacate’, ‘Melancia’, ‘Melancia’, ‘Uva’], deverá retornar { Melancia: 3, Abacate: 1, Uva: 1 } quando passado como argumento para a função.
+
+// Em seguida, imprima esse resultado na tela com uma mensagem no seguinte formato: Sua cesta possui: x Melancias, x Abacates...
+
+// const basket = [
+//   'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+//   'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+//   'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+//   'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+//   'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+//   'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+//   'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+//   'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+//   'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+//   'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+//   'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+//   'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+//   'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+//   'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+//   'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+//   'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+//   'Banana', 'Pera', 'Abacate', 'Uva',
+// ];
+
+
+// const result = {};
+
+// for (let index = 0; index < basket.length; index += 1){
+//   const fruit = basket[index];
+//   if (!result[fruit]) result[fruit] = 0;  //Quinto Passo
+//   result[fruit] += 1;
+// }
+
+// const summaries = [];
+
+// for (fruit in result) {    //sexto passo
+//   let message = `${result[fruit]} ${fruit}`;   //setimo passo
+//   if (result[fruit] > 1) message += 's';
+//   summaries.push(message);   //oitavo passo
+// }
+
+// console.log(`Sua cesta possui: ${summaries.join(', ')}.`);
+
+
+//exercicio Bonus 04
+//Usando o objeto abaixo, faça o que for pedido a seguir:
+//Acesse as chaves nome, sobrenome, andar e apartamento do último morador do blocoDois e faça um console.log no seguinte formato: “O morador do bloco 2 de nome Zoey Brooks mora no 1° andar, apartamento 101”.
+
+// Utilize o for para imprimir o nome completo de todos os moradores do bloco 1, acessando suas chaves nome e sobrenome. Depois faça o mesmo para os moradores do bloco 2.
+
+let moradores = {
+  blocoUm: [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+  ],
+  blocoDois: [
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ],
+};
+
+let moradoresBlocoDois = moradores.blocoDois;
+let ultimoMoradorBlocoDois = moradoresBlocoDois[moradoresBlocoDois.length -1];
+
+console.log('O morador do bloco 2 de nome ' + ultimoMoradorBlocoDois.nome + ' ' + ultimoMoradorBlocoDois.sobrenome + ' ' + ultimoMoradorBlocoDois.andar + '° andar, apartamento ' + ultimoMoradorBlocoDois.apartamento);
+
+for (let index = 0; index < moradores.blocoUm.length; index += 1) {
+  console.log(moradores.blocoUm[index].nome + ' ' + moradores.blocoUm[index].sobrenome);
 }
 
-console.log(check('trybe', 'be'));
-console.log(check('joaofernando', 'fernan'));
+for (let index = 0; index < moradores.blocoDois.length; index += 1) {
+  console.log(moradores.blocoDois[index].nome + ' ' + moradores.blocoDois[index].sobrenome);
+}
