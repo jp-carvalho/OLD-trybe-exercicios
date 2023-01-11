2adbcf2a058d151f85a9e2b65b773a021259fc24
joao-paulo-carvalho <desenhojp@gmail.com>
2023-01-11 14:17:37 -0300

    exercicios dia 6.1

--- 01-fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-01-forms/dia-01-forms/dia-01-forms/exercicios-dia/script.js
+++ 01-fundamentos/secao-06-html-e-css-forms-flexbox-e-responsivo/dia-01-forms/dia-01-forms/dia-01-forms/exercicios-dia/script.js
@@ -20,12 +20,31 @@ function enablaSendBtn () {
 }
 
 const textValidation = () => {
-  
+  const name = document.getElementById('name').value.length;
+  const invalidName = name < 10 || name > 40;
+
+  const email = document.getElementById('email').value.length;
+  const invalidEmail = email < 10 || email > 50;
+
+  const why = document.getElementById('why').value.length;
+  const invalidWhy = why > 500;
+
+  if (invalidName || invalidEmail || invalidWhy) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 window.onload = function () {
   sendBtn.addEventListener('click', (event) => {
     event.preventDefault();
+    const validation = textValidation();
+    if (validation === false) {
+      alert('Dados inválidos');
+    } else {
+      alert('Dados enviados com sucesso! Obrigado por participar do concirto TrybeTrip.');
+    }
   });
   const eraseBtn = document.getElementById('reset');
   eraseBtn.addEventListener('click', clearAll);
