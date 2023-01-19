8463f2ac8e89bee0c09a984cac0fa68220d0a38f
joao-paulo-carvalho <desenhojp@gmail.com>
2023-01-19 14:45:53 -0300

    fim exercicios

--- 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excessoes-e-manipulacao-de-objetos/script.js
+++ 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excessoes-e-manipulacao-de-objetos/script.js
@@ -168,65 +168,147 @@
 // Modifique o nome da pessoa compradora para Luiz Silva;
 // Modifique o valor total da compra para R$ 50,00.
 
-const order = {
-  name: 'Rafael Andrade',
-  phoneNumber: '11-98763-1416',
-  address: {
-    street: 'Rua das Flores',
-    number: '389',
-    apartment: '701',
-  },
-  order: {
-    pizza: {
-      marguerita: {
-        amount: 1,
-        price: 25,
-      },
-      pepperoni: {
-        amount: 1,
-        price: 20,
-      },
-    },
-    drinks: {
-      coke: {
-        type: 'Coca-Cola Zero',
-        price: 10,
-        amount: 1,
-      },
-    },
-    delivery: {
-      deliveryPerson: 'Ana Silveira',
-      price: 5,
-    },
-  },
-  payment: {
-    total: 60,
-  },
+// const order = {
+//   name: 'Rafael Andrade',
+//   phoneNumber: '11-98763-1416',
+//   address: {
+//     street: 'Rua das Flores',
+//     number: '389',
+//     apartment: '701',
+//   },
+//   order: {
+//     pizza: {
+//       marguerita: {
+//         amount: 1,
+//         price: 25,
+//       },
+//       pepperoni: {
+//         amount: 1,
+//         price: 20,
+//       },
+//     },
+//     drinks: {
+//       coke: {
+//         type: 'Coca-Cola Zero',
+//         price: 10,
+//         amount: 1,
+//       },
+//     },
+//     delivery: {
+//       deliveryPerson: 'Ana Silveira',
+//       price: 5,
+//     },
+//   },
+//   payment: {
+//     total: 60,
+//   },
+// };
+
+// const customerInfo = (order) => {
+//   // Adicione abaixo as informações necessárias.
+//   const address = 'address';
+//   const deliveryPerson = order.order.delivery.deliveryPerson;
+//   const customerName = order.name;
+//   const customerPhone = order.phoneNumber;
+//   const street = order.address.street;
+//   const number = order.address.number;
+//   const apartment = order.address.apartment;
+
+//   console.log(`Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone ${customerPhone}, R. ${street}, Nº: ${number}, AP: ${apartment}`);
+// };
+
+// customerInfo(order);
+
+// const orderModifier = (order) => {
+//   // Adicione abaixo as informações necessárias.
+//   const newBuyer = order.name = 'Luiz Silva';
+//   const newTotal = order.payment.total = '50';
+//   const pizzas = Object.keys(order.order.pizza);
+//   const drink = order.order.drinks.coke.type;
+
+//   console.log(`Olá, ${newBuyer}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drink} é R$${newTotal},00.`);
+// };
+
+// orderModifier(order);
+
+//=================================================================================
+//EXERCICIO 03
+
+
+
+
+// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
 };
 
-const customerInfo = (order) => {
-  // Adicione abaixo as informações necessárias.
-  const address = 'address';
-  const deliveryPerson = order.order.delivery.deliveryPerson;
-  const customerName = order.name;
-  const customerPhone = order.phoneNumber;
-  const street = order.address.street;
-  const number = order.address.number;
-  const apartment = order.address.apartment;
-
-  console.log(`Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone ${customerPhone}, R. ${street}, Nº: ${number}, AP: ${apartment}`);
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
 };
 
-customerInfo(order);
-
-const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
-  const newBuyer = order.name = 'Luiz Silva';
-  const newTotal = order.payment.total = '50';
-  const pizzas = Object.keys(order.order.pizza);
-  const drink = order.order.drinks.coke.type;
-
-  console.log(`Olá, ${newBuyer}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drink} é R$${newTotal},00.`);
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
 };
 
-orderModifier(order);
+// Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+const addNewKey = (object, key, value) => {
+  object[key] = value;
+};
+
+addNewKey(lesson2, 'turno', 'noite');
+console.log(lesson2);
+
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+const listKey = (object) => Object.keys(object);
+console.log(listKey(lesson1));
+
+// Crie uma função para mostrar o tamanho de um objeto.
+const sizeObject = (object) => Object.keys(object).length;
+console.log(sizeObject(lesson1));
+
+// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+const objectValue = (object) => Object.values(object);
+console.log(objectValue(lesson1));
+
+// Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves lesson1, lesson2 e lesson3.
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons);
+
+// Usando o objeto criado no tópico anterior, crie uma função que retorne o número total de estudantes em todas as aulas.
+const studentSum = (object) => {
+  let total = 0;
+  const keys = Object.keys(object);
+
+  for (index in keys) {
+    total += object[keys[index]].numeroEstudantes;
+  }
+  return total;
+}
+console.log(studentSum(allLessons));
+
+// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+const getValueByNumber = (object, number) => Object.values(object)[number];
+console.log(getValueByNumber(lesson1, 2));
+
+// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles o objeto, o nome da chave e o valor da chave.
+const checkObject = (object, key, value) => {
+  const entries = Object.entries(object);
+  let isEqual = false;
+  for (let index in entries) {
+    if (entries[index][0] === key && entries[index][1] === value) {
+      isEqual = true;
+    }
+  }
+  return isEqual;
+}
+console.log(checkObject(lesson2, 'professor', 'Carlos'));
