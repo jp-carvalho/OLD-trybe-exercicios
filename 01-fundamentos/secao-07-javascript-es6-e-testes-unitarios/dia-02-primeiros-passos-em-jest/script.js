23995a0a920026979c367df742a0eb53f04f2cf1
joao-paulo-carvalho <desenhojp@gmail.com>
2023-01-20 12:08:23 -0300

    exercicio 04

--- 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.js
+++ 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.js
@@ -27,41 +27,63 @@
 //===================================================================================
 
 //EXERCICIO 03
-const mapString = (objectMap, string) => {
-  const splitString = string.split('');
-  const mappedArray = [];
-  for (let index = 0; index < splitString.length; index += 1) {
-    const character = splitString[index];
-    const mappedValue = objectMap[character];
+// const mapString = (objectMap, string) => {
+//   const splitString = string.split('');
+//   const mappedArray = [];
+//   for (let index = 0; index < splitString.length; index += 1) {
+//     const character = splitString[index];
+//     const mappedValue = objectMap[character];
     
-    if (mappedValue) {
-      mappedArray.push(mappedValue);
-    } else {
-      mappedArray.push(character);
-    }
+//     if (mappedValue) {
+//       mappedArray.push(mappedValue);
+//     } else {
+//       mappedArray.push(character);
+//     }
+//   }
+//   return mappedArray.join('');
+// }
+// const encode = (string) => {
+//   const map = {
+//     a: 1,
+//     e: 2,
+//     i: 3,
+//     o: 4,
+//     u: 5,
+//   };
+//   return mapString(map, string);
+// }
+// const decode = (string) => {
+//   const map = {
+//     1: 'a',
+//     2: 'e',
+//     3: 'i',
+//     4: 'o',
+//     5: 'u',
+//   };
+//   return mapString(map, string);
+// }
+
+// const functions = { encode, decode };
+// module.exports = functions;
+
+
+//===================================================================================
+
+//EXERCICIO 04
+
+const techList = (array, name) => {
+  if(array.length === 0) return 'Vazio!';
+
+  const sortArray = array.sort();
+  const sortTechList = [];
+
+  for (let index = 0; index < sortArray.length; index += 1) {
+    sortTechList.push( {
+      tech: sortArray[index],
+      name: name,
+    });
   }
-  return mappedArray.join('');
-}
-const encode = (string) => {
-  const map = {
-    a: 1,
-    e: 2,
-    i: 3,
-    o: 4,
-    u: 5,
-  };
-  return mapString(map, string);
-}
-const decode = (string) => {
-  const map = {
-    1: 'a',
-    2: 'e',
-    3: 'i',
-    4: 'o',
-    5: 'u',
-  };
-  return mapString(map, string);
+  return sortTechList;
 }
 
-const functions = { encode, decode };
-module.exports = functions;
+module.exports = techList;
--- 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.test.js
+++ 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.test.js
@@ -26,52 +26,97 @@
 //===================================================================================
 
 //EXERCICIO 03
-const {encode, decode} = require('./script');
+// const {encode, decode} = require('./script');
 
-describe('Testa a função encode e decode', () => {
-  test('a função encode é definida', () => {
-    expect(encode).toBeDefined();
+// describe('Testa a função encode e decode', () => {
+//   test('a função encode é definida', () => {
+//     expect(encode).toBeDefined();
+//   });
+//   test('encode é uma função', () => {
+//     expect(typeof encode).toEqual('function');
+//   });
+//   test('converte a vogal "a" no número 1', () => {
+//     expect(encode('ana')).toEqual('1n1');
+//   });
+//   test('converte a vogal "e" no número 2', () => {
+//     expect(encode('ele')).toEqual('2l2')
+//   });
+//   test('converte a vogal "i" no número 3', () => {
+//     expect(encode('xixi')).toEqual('x3x3');
+//   });
+//   test('converte a vogal "o" no número 4', () => {
+//     expect(encode('ovo')).toEqual('4v4');
+//   });
+//   test('converte a vogal "u" no número 5', () => {
+//     expect(encode('urubu')).toEqual('5r5b5');
+//   });
+//   test('o retorno da funcão tem o mesmo número de caracteres', () => {
+//     expect(encode('trybe').length).toEqual(5);
+//   });
+//   test('a função decode é definida', () => {
+//     expect(decode).toBeDefined();
+//   });
+//   test('decode é uma função', () => {
+//     expect(typeof decode).toEqual('function');
+//   });
+//   test('converte o número 1 na vogal "a"', () => {
+//     expect(decode('1n1')).toEqual('ana');
+//   });
+//   test('converte o número 2 na vogal "e"', () => {
+//     expect(decode('2l2')).toEqual('ele');
+//   });
+//   test('converte o número 3 na vogal "i"', () => {
+//     expect(decode('x3x3')).toEqual('xixi');
+//   });
+//   test('converte o número 4 na vogal "o"', () => {
+//     expect(decode('4v4')).toEqual('ovo');
+//   });
+//   test('converte o número 5 na vogal "u"', () => {
+//     expect(decode('5r5b5')).toEqual('urubu');
+//   });
+// });
+
+
+//===================================================================================
+
+//EXERCICIO 04
+
+const techList = require('./script');
+
+describe('Testa a função techList', () => {
+  it('Testa se a função techList é definida', () => {
+    expect(techList).toBeDefined();
   });
-  test('encode é uma função', () => {
-    expect(typeof encode).toEqual('function');
+  it('Testa se techList é uma função', () => {
+    expect(typeof techList).toBe('function');
   });
-  test('converte a vogal "a" no número 1', () => {
-    expect(encode('ana')).toEqual('1n1');
+  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+    expect(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')).toEqual([
+      {
+        tech: 'CSS',
+        name: 'Lucas'
+      },
+      {
+        tech: 'HTML',
+        name: 'Lucas'
+      },
+      {
+        tech: 'JavaScript',
+        name: 'Lucas'
+      },
+      {
+        tech: 'Jest',
+        name: 'Lucas'
+      },
+      {
+        tech: 'React',
+        name: 'Lucas'
+      }
+    ]);
   });
-  test('converte a vogal "e" no número 2', () => {
-    expect(encode('ele')).toEqual('2l2')
-  });
-  test('converte a vogal "i" no número 3', () => {
-    expect(encode('xixi')).toEqual('x3x3');
-  });
-  test('converte a vogal "o" no número 4', () => {
-    expect(encode('ovo')).toEqual('4v4');
-  });
-  test('converte a vogal "u" no número 5', () => {
-    expect(encode('urubu')).toEqual('5r5b5');
-  });
-  test('o retorno da funcão tem o mesmo número de caracteres', () => {
-    expect(encode('trybe').length).toEqual(5);
-  });
-  test('a função decode é definida', () => {
-    expect(decode).toBeDefined();
-  });
-  test('decode é uma função', () => {
-    expect(typeof decode).toEqual('function');
-  });
-  test('converte o número 1 na vogal "a"', () => {
-    expect(decode('1n1')).toEqual('ana');
-  });
-  test('converte o número 2 na vogal "e"', () => {
-    expect(decode('2l2')).toEqual('ele');
-  });
-  test('converte o número 3 na vogal "i"', () => {
-    expect(decode('x3x3')).toEqual('xixi');
-  });
-  test('converte o número 4 na vogal "o"', () => {
-    expect(decode('4v4')).toEqual('ovo');
-  });
-  test('converte o número 5 na vogal "u"', () => {
-    expect(decode('5r5b5')).toEqual('urubu');
+  it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+    expect(techList([], 'Lucas')).toBe('Vazio!');
   });
 });
+
+module.exports = techList;
