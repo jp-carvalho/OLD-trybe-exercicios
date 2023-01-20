0085f38367fd182b471e6d0de183a480fa11d669
joao-paulo-carvalho <desenhojp@gmail.com>
2023-01-19 22:17:02 -0300

    exercicio 02

--- 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.js
+++ 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.js
@@ -1,12 +1,25 @@
-function myRemove(arr, item) {
-  let newArr = [];
-  for (let index = 0; index < arr.length; index += 1) {
-    if (item !== arr[index]) {
-      newArr.push(arr[index]);
-    }
-  }
-  return newArr;
+//EXERCICIO 01
+// function myRemove(arr, item) {
+//   let newArr = [];
+//   for (let index = 0; index < arr.length; index += 1) {
+//     if (item !== arr[index]) {
+//       newArr.push(arr[index]);
+//     }
+//   }
+//   return newArr;
+// }
+
+// module.exports = myRemove;
+
+//===================================================================================
+
+//EXERCICIO 02
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
 }
 
-module.exports = myRemove;
-// implemente seus testes aqui
+module.exports = myFizzBuzz;
--- 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.test.js
+++ 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.test.js
@@ -1,8 +1,24 @@
-const myRemove = require('./script.js');
+//EXERCICIO 01
+// const myRemove = require('./script.js');
 
-describe('testa a função myRemove', () => {
-  test('deve retornar um array removendo o item correto', () => {});
-    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
-    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
-    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
-});
+// describe('testa a função myRemove', () => {
+//   test('deve retornar um array removendo o item correto', () => {});
+//     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+//     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+//     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+// });
+
+//===================================================================================
+
+//EXERCICIO 02
+const myFizzBuzz = require('./script');
+
+describe('Testa a função myFizzBuzz', () => {
+  test('verifica, se de acordo com o parâmetro recebido, retorna o valor esperado', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+    expect(myFizzBuzz(9)).toBe('fizz');
+    expect(myFizzBuzz(5)).toBe('buzz');
+    expect(myFizzBuzz(2)).toBe(2);
+    expect(myFizzBuzz('a')).toBe(false);
+  })
+})
