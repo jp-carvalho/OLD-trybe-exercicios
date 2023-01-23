f5a28e14373317750fb51a5b6c0757ad3b6f9abf
joao-paulo-carvalho <desenhojp@gmail.com>
2023-01-23 10:16:37 -0300

    exercicio 01

--- 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/script.js
+++ 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/script.js
@@ -1,18 +1,28 @@
-const createItem = (name, unit, price, quantity) => {
-  if (typeof name !== 'string') {
-    throw new Error('O nome do item deve ser uma string');
+// const createItem = (name, unit, price, quantity) => {
+//   if (typeof name !== 'string') {
+//     throw new Error('O nome do item deve ser uma string');
+//   }
+//   if (price <= 0) {
+//     throw new Error('O preço do item deve ser maior que zero');
+//   }
+//   if (quantity === undefined) {
+//     quantity = 0;
+//   }
+//   return {
+//     name: name,
+//     quantity: quantity,
+//     unit: unit,
+//     price: price,
+//   };
+// }
+// module.exports = createItem;
+
+// exercicio 01
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
   }
-  if (price <= 0) {
-    throw new Error('O preço do item deve ser maior que zero');
-  }
-  if (quantity === undefined) {
-    quantity = 0;
-  }
-  return {
-    name: name,
-    quantity: quantity,
-    unit: unit,
-    price: price,
-  };
+
+  return a + b;
 }
-module.exports = createItem;
+module.exports = sum;
--- 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/script.test.js
+++ 01-fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-matchers-e-cobertura-de-codigo/script.test.js
@@ -1,22 +1,37 @@
-const createItem = require('./script');
-describe('a função createItem', () => {
-  it('cria um item válido', () => {
-    expect(createItem('banana', 2, 100, 10)).toEqual({name: 'banana', unit: 2, price: 100, quantity: 10});
-  });
-  it('utiliza zero como quantidade padrão', () => {
-    expect(createItem('banana', 2, 10)).toEqual({name: 'banana', unit: 2, price: 10, quantity: 0})
-  });
-  it('Lança um erro quando não recebe parâmetros', () => {
-    expect()
-  });
-  it('Lança um erro se o nome do item não é uma string', () => {
-    expect( () => createItem(2, 2, 100, 10)).toThrow()
-  });
-  it('Lança um erro se o preço é negativo', () => {
-    expect( () => createItem('banana', 2, -10, 10)).toThrow()
-  });
+// const createItem = require('./script');
+// describe('a função createItem', () => {
+//   it('cria um item válido', () => {
+//     expect(createItem('banana', 2, 100, 10)).toEqual({name: 'banana', unit: 2, price: 100, quantity: 10});
+//   });
+//   it('utiliza zero como quantidade padrão', () => {
+//     expect(createItem('banana', 2, 10)).toEqual({name: 'banana', unit: 2, price: 10, quantity: 0})
+//   });
+//   it('Lança um erro quando não recebe parâmetros', () => {
+//     expect()
+//   });
+//   it('Lança um erro se o nome do item não é uma string', () => {
+//     expect( () => createItem(2, 2, 100, 10)).toThrow()
+//   });
+//   it('Lança um erro se o preço é negativo', () => {
+//     expect( () => createItem('banana', 2, -10, 10)).toThrow()
+//   });
 
-  it('Lança um erro se o preço é zero', () => {
-    expect( () => createItem('banana', 2, 0, 10)).toThrow()
+//   it('Lança um erro se o preço é zero', () => {
+//     expect( () => createItem('banana', 2, 0, 10)).toThrow()
+//   });
+// });
+
+//exercicio 01
+const sum = require('./script');
+
+describe('Testa a função sum', () => {
+  it('Teste se o resultado da soma', () =>{
+    expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);
   });
+  it('Teste erro caso seja lançada uma string', () => {
+    expect( () => sum(4, '5')).toThrowError();
+    expect( () => sum(4, '5')).toThrowError('parameters must be numbers');
+  })
 });
+
