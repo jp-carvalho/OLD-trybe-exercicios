906ff5437312472962380a67b2fd112f602e807c
joao-paulo-carvalho <desenhojp@gmail.com>
2023-01-27 14:32:00 -0300

    criação fundamentos/8.3

--- 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-02-sort-e-map/scriptCourse.js
+++ 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-02-sort-e-map/scriptCourse.js
@@ -26,12 +26,16 @@
 
 // console.log(people);
 
-const persons = [
-  { firstName: 'Maria', lastName: 'Ferreira' },
-  { firstName: 'João', lastName: 'Silva' },
-  { firstName: 'Antonio', lastName: 'Cabral' },
-];
+// const persons = [
+//   { firstName: 'Maria', lastName: 'Ferreira' },
+//   { firstName: 'João', lastName: 'Silva' },
+//   { firstName: 'Antonio', lastName: 'Cabral' },
+// ];
 
-const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+// const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
 
-console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+// console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
