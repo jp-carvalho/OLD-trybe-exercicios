ac8345893e0d8ff8cd62f5227bf20a5ce9e283eb
joao-paulo-carvalho <desenhojp@gmail.com>
2023-01-31 14:21:30 -0300

    exercicios course fundamentos/8.2

--- 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-02-sort-e-map/scriptCourse.js
+++ 01-fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-02-sort-e-map/scriptCourse.js
@@ -36,6 +36,108 @@
 
 // console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
 
-const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
-const prices = [2.99, 3.99, 1.5, 2];
+// =======================================================================================================
 
+// const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+// const prices = [2.99, 3.99, 1.5, 2];
+
+// const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+//   { [product]: listPrices[index] }
+// ));
+
+// const listProducts = updateProducts(products, prices);
+// console.log(listProducts);
+
+// =======================================================================================================
+// Nome de todos os estudantes que estudam de manhã com map e filter
+
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: '59' },
+      { name: 'Português', nota: '80' },
+      { name: 'Química', nota: '78' },
+      { name: 'Biologia', nota: '92' },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '76' },
+      { name: 'Português', nota: '90' },
+      { name: 'Química', nota: '70' },
+      { name: 'Biologia', nota: '80' },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '91' },
+      { name: 'Português', nota: '85' },
+      { name: 'Química', nota: '92' },
+      { name: 'Biologia', nota: '90' },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '70' },
+      { name: 'Português', nota: '70' },
+      { name: 'Química', nota: '60' },
+      { name: 'Biologia', nota: '50' },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '80' },
+      { name: 'Português', nota: '82' },
+      { name: 'Química', nota: '79' },
+      { name: 'Biologia', nota: '75' },
+    ],
+  },
+];
+
+// // O filter possui a mesma estrutura do forEach, map, entre outros, e é responsável por retornar um array filtrado de acordo com alguma condição. Então o map itera pelo array filtrado e retorna o nome e sobrenome da pessoa estudante. 
+
+// const allNameStudents = estudantes.filter((estudante) => (estudante.turno === 'Manhã')).map((estudante) => `${estudante.nome} ${estudante.sobrenome}`);
+// console.log(allNameStudents);
+
+// =======================================================================================================
+
+// Buscar um estudante pelo nome e retornar a situação dele em cada matéria:
+const studentStats = (name, students) => {
+  const studentInfo = students.find((student) => student.nome === name );
+  return studentInfo.materias.map((materia) => (
+    `${materia.name} ${(materia.nota >= 60) ? 'Aprovado' : 'Reprovado'}`
+  ));
+};
+console.log(studentStats('Natalia', estudantes));
