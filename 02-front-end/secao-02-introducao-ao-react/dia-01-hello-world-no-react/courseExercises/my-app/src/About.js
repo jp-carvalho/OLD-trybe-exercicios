f45711a6244ca4e1944f25cbf77aa1f69f8b3ef6
joao-paulo-carvalho <desenhojp@gmail.com>
2023-02-27 19:43:48 -0300

    para fixar front end 2.1

---
+++ 02-front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/courseExercises/my-app/src/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class AboutMe extends React.Component {
+  render() {
+    return (
+      <>
+      <h1>João Paulo</h1>
+      <p>Não sei o que escrever sobre mim, mas fica aqui essa frase de teste kkkk</p>
+      <h2>Minhas Habilidades</h2>
+      <ol>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Artesão</li>
+      </ol>
+      </>
+    )
+  }
+}
+
+export default AboutMe;
