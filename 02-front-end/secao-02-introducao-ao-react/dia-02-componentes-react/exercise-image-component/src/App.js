8418599353dc7b2ee59668ee0a5351958bee4416
joao-paulo-carvalho <desenhojp@gmail.com>
2023-03-01 02:05:23 -0300

    feat: para fixar frontend/2.2

---
+++ 02-front-end/secao-02-introducao-ao-react/dia-02-componentes-react/exercise-image-component/src/App.js
@@ -0,0 +1,10 @@
+import Image from './Image';
+import staringCat from './staringCat.jpg'
+
+function App() {
+  return (
+    <Image source={staringCat} alternativeText="Cute cat staring"/>
+  );
+}
+
+export default App;
