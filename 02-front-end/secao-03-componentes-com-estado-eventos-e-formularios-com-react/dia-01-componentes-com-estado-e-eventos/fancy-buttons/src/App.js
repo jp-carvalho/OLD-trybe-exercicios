87c340d38c9eae91bfc4b8f17cd95c0622ac9bc4
joao-paulo-carvalho <desenhojp@gmail.com>
2023-03-03 09:35:11 -0300

    course exercicio dois: constructor, this e bind

--- 02-front-end/secao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 02-front-end/secao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,14 +1,36 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function handleClick() {
-  console.log('clicou');
-}
+class App extends Component {
+  constructor() {
+    super()
+    this.handleClickOne = this.handleClickOne.bind(this)
+    this.handleClickTwo = this.handleClickTwo.bind(this)
+    this.handleClickThree = this.handleClickThree.bind(this)
+  }
 
-function App() {
-  return (
-    <button onClick={handleClick}>Meu botão</button>
-  );
+  handleClickOne() {
+    console.log('"this do botão 1"', this);
+  }
+
+  handleClickTwo() {
+    console.log('"this do botão 2"', this);
+  }
+
+  handleClickThree() {
+    console.log('"this do botão 3"', this);
+  }
+
+  render(){
+    return (
+      <>
+        <button onClick={this.handleClickOne}>Primeiro botão</button>
+        <button onClick={this.handleClickTwo}>Segundo botão</button>
+        <button onClick={this.handleClickThree}>terceiro botão</button>
+      </>
+      );
+    
+  }
 }
 
 export default App;
