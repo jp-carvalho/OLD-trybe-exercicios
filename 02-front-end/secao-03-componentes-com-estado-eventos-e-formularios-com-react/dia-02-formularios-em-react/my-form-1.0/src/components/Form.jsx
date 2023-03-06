6ecdacb30343fd5d9b45ff34e1e39f1820912f21
joao-paulo-carvalho <desenhojp@gmail.com>
2023-03-05 23:12:50 -0300

    fim exercicios domingo

--- 02-front-end/secao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-em-react/my-form-1.0/src/components/Form.jsx
+++ 02-front-end/secao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-em-react/my-form-1.0/src/components/Form.jsx
@@ -5,45 +5,84 @@ class Form extends Component {
     super()
 
     this.state ={
+      name: '',
       email: '',
+      age: '',
+      anecdote: '',
+      terms: false,
     }
     this.handleChange = this.handleChange.bind(this)
   }
 
-handleChange(event) {
+handleChange({ target }) {
+  const { name } = target;
+  const value = (target.type === 'checkbox') ? target.checked : target.value;
+
   this.setState({
-    email: event.target.value,
+    [name]: value,
   })
 }
 
   render() {
-    const { email } = this.state;
+    const { name, email, age, anecdote, terms } = this.state;
 
     return(
       <form >
-        <label htmlFor="name">
-          Nome:
-          <input type="text" name='name' id='name'/>
-        </label>
+         <label htmlFor="name">
+            Nome:
+            <input
+              id="name"
+              name="name"
+              type="text"
+              onChange={ this.handleChange }
+              value={ name }
+            />
+          </label>
 
         <label htmlFor="email">
           Email:
-          <input type="email" name='email' id='email'onChange={ this.handleChange} value={ email }/>
+          <input 
+            type="email" 
+            name='email' 
+            id='email'
+            onChange={ this.handleChange} 
+            value={ email }/>
         </label>
 
         <label htmlFor="age">
           Idade:
-          <select name='age' id='age' defaultValue=''>
+          <select 
+            name='age' 
+            id='age' 
+            onChange={ this.handleChange } 
+            value={ age }
+          >
             <option value="">Selecione</option>
             <option value="adult">Maior que 18</option>
             <option value="underage">Menor que 18</option>
           </select>
         </label>
 
+        <label htmlFor="terms">
+          <input 
+            type="checkbox" 
+            id='terms' 
+            name='terms'
+          />
+          Concordo com os termos
+        </label>
+
         <label htmlFor="anecdote">
           Anedota:
-          <textarea name="anecdote" id="anecdote"></textarea>
+          <textarea 
+            name="anecdote" 
+            id="anecdote"
+            onChange={ this.handleChange }
+            value={ anecdote }
+          />
         </label>
+
+        <input type="file" />
 
 
       </form>
---
+++ 02-front-end/secao-03-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-em-react/my-form-1.0/src/components/SelectInput.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SelectInput extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default SelectInput
